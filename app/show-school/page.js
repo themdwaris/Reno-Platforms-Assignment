5d7482e2ef224673c05effb8ec4857a1d5366dff
Mohammad Waris <mdtech9900@gmail.com>
2026-02-21 14:58:36 +0530

    Improve some code

--- app/show-school/page.js
+++ app/show-school/page.js
@@ -1,11 +1,17 @@
 import SchoolList from "@/components/SchoolList";
+import pool from "@/config/db";
 
 // ***********Server side component************
 
 export const revalidate = 60;
 const ShowSchools = async () => {
-  const res = await fetch("http://localhost:3000/api/schools");
-  const { data } = await res.json();
+  const [rows] = await pool.query(
+      `SELECT id, name, address, city, state, email, image, created_at 
+       FROM schools 
+       ORDER BY created_at DESC`
+    );
+
+    const data = rows
 
   return (
     <>
